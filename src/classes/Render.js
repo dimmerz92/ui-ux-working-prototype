29cf34162e8251e54d1166565b5a1d694a709ff7
dimmerz92 <andrew.weymes@outlook.com>
2023-04-26 19:08:31 +0800

    add target to render and JSDoc

--- src/classes/Render.js
+++ src/classes/Render.js
@@ -2,9 +2,17 @@ class Render {
     constructor() {
         this.DOM = document.getElementsByTagName("main")[0];
     }
-    render(content) {
-        this.DOM.innerHTML = "";
-        this.DOM.appendChild(content);
+
+    /**
+     * @function render
+     * @memberof class:Render
+     * @param {HTMLElement} content - The content to be rendered
+     * @param {string} [target=] - OPTIONAL: a string containing the id of the parent element, defaults to the 'main' tag if no argument given
+     */
+    render(content, target) {
+        target = (!target ? this.DOM : document.getElementById(target));
+        target.innerHTML = "";
+        target.appendChild(content);
     }
 }
 
