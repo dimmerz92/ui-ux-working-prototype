59186903c7e34cd3b3cb652ae0e5e0fe25089fdc
dimmerz92 <andrew.weymes@outlook.com>
2023-04-26 22:13:47 +0800

    fix image handling

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,8 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }
+        type: 'asset/resource'
+      },
     ],
   }
 };
