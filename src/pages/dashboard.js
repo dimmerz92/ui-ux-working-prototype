78cec4989c1cc0cab688b9264397d89648a73446
dimmerz92 <andrew.weymes@outlook.com>
2023-05-20 16:45:05 +0800

    add dashboard page and style

---
+++ src/pages/dashboard.js
@@ -0,0 +1,79 @@
+import Render from "../classes/Render";
+
+function dashboard(workspacesJSON) {
+    // landing page wrapper
+    const main = document.createElement("div")
+    main.id = "dashboard-page"
+
+    // search
+    const search = document.createElement("div");
+    search.id = "dash-search";
+    const txtInput = document.createElement("input");
+    txtInput.type = "text";
+    txtInput.placeholder = "Search for a company (e.g., CBA)";
+    search.appendChild(txtInput);    
+    main.appendChild(search);
+
+    // ASX chart
+    const chart = document.createElement("div");
+    chart.id = "dash-chart";
+    const chartScript = document.createElement("script");
+    chartScript.type = "text/javascript";
+    chartScript.src = "https://s3.tradingview.com/tv.js";
+    chartScript.onload = function() {
+        const chartScript2 = document.createElement("script");
+        chartScript2.type = "text/javascript";
+        chartScript2.innerHTML = `
+            new TradingView.widget(
+            {
+                "autosize": true,
+                "symbol": "ASX:XJO",
+                "interval": "D",
+                "timezone": "Etc/UTC",
+                "theme": "light",
+                "style": "1",
+                "locale": "en",
+                "toolbar_bg": "#f1f3f6",
+                "enable_publishing": false,
+                "allow_symbol_change": false,
+                "container_id": "dash-chart"
+            });`;
+        chart.appendChild(chartScript2)}
+    main.appendChild(chartScript);
+    main.appendChild(chart);
+
+    // workspaces
+    const workspaces = document.createElement("div");
+    workspaces.id = "dash-workspaces";
+    const h2 = document.createElement("h2");
+    h2.textContent = "Workspaces";
+    workspaces.appendChild(h2);
+    const bench = document.createElement("div");
+    bench.id = "bench";
+    if (workspacesJSON.cards.length) {
+        const cards = Render.buildWorkspaceCard(workspacesJSON);
+        // Do something
+    }
+    const newCard = document.createElement("div");
+    newCard.id = "dash-add-workspace";
+    newCard.classList.add("card");
+    const linkAction = document.createElement("a");
+    linkAction.href = "javascript:void(0);";
+    const div = document.createElement("div");
+    const p1 = document.createElement("p");
+    p1.id = "big-plus";
+    p1.textContent = "+";
+    div.appendChild(p1);
+    const p2 = document.createElement("p");
+    p2.textContent = "Add new workspace";
+    div.appendChild(p2);
+    linkAction.appendChild(div);
+    newCard.appendChild(linkAction);
+    bench.appendChild(newCard);
+    workspaces.appendChild(bench);
+    main.appendChild(workspaces);
+
+    return main;
+}
+
+export default dashboard;
