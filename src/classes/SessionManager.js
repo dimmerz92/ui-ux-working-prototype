6ad6465e1ac18b510e3c373bbafb184acab0f342
dimmerz92 <andrew.weymes@outlook.com>
2023-05-21 13:56:39 +0800

    improve session manager class

--- src/classes/SessionManager.js
+++ src/classes/SessionManager.js
@@ -1,18 +1,21 @@
 class SessionManager {
     constructor() {
         this.loggedIn = false;
+        this.username = false;
     }
 
     status() {
         return this.loggedIn;
     }
 
-    logIn() {
+    logIn(username) {
         this.loggedIn = true;
+        this.username = username;
     };
 
     logOut() {
         this.loggedIn = false;
+        this.username = false;
     }
 }
 
