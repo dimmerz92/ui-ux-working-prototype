8016d1724b4607ce50dfa6a7d207ca7c0065e62f
dimmerz92 <andrew.weymes@outlook.com>
2023-05-22 16:53:31 +0800

    add adhoc files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './style.css'
+import './styles/style.css';
+import './styles/landing.css';
+import './styles/progress.css';
+import './styles/login.css';
+import './styles/dashboard.css';
+import './styles/workspaces.css';
+import Handler from './classes/Handler';
+
+const handler = new Handler();
